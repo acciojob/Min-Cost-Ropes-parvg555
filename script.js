e2e8056094dfb676f04748598e6a11693adcd3f9
Parv Gupta <Parvg555@gmail.com>
2023-02-21 14:52:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
 function mincost(arr)
-{ 
-//write your code here
-// return the min cost
-  
+{
+  //write your code here
+    arr.sort((a,b)=>a-b)
+
+    let cost = 0
+    //console.log(arr)
+    while(arr.length>1){
+        let first = arr.shift()
+        let second = arr.shift()
+
+        cost+=first+second
+
+        arr.push(first+second)
+        arr.sort((a,b)=>a-b)
+      //  console.log(arr)
+    }
+
+    return cost
 }
 
 module.exports=mincost;
